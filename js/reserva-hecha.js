880fbd80fc6d5d571cf109453ed2d1acf61c6385
Erasmo23 <josue.menendez23@gmail.com>
2015-11-03 07:52:49 -0600

    :D

--- js/reserva-hecha.js
+++ js/reserva-hecha.js
@@ -1,5 +1,6 @@
 var total;
 var reserva;
+var Reservas;
 
 //creando el evento load de la ventana para prepara las imagenes y eventos que se crearan
 if(window.addEventListener){//navegadores recientes
@@ -13,6 +14,13 @@ function iniciar () {
 	Reserva= localStorage.getItem("TempMostrar");
 	Reserva= JSON.parse(Reserva);
 	total= localStorage.getItem("Total");
+	Reservas= localStorage.getItem("Reserva");
+	Reservas= JSON.parse(Reservas);
+	ultimareservacion();
+	todasResservaciones();
+}
+
+function ultimareservacion (){
 	var hor="";
 	switch(Reserva.Horario) {
     case 1:
@@ -33,8 +41,8 @@ function iniciar () {
     default:
          hor="11:00 am - 12:30 pm.";
          break;
-}
-	var cadena= "<table id= \"Datos\"> <thead> <tr><th colspan=\"2\">Datos de la Reserva Realizada</th></tr></thead><tbody>";
+	}
+	var cadena= "<table id= \"Datos\"> <thead> <tr><th colspan=\"2\">Datos de la ultima Reserva Realizada</th></tr></thead><tbody>";
 		cadena+= "<tr><td>Nombre completo:</td><td>" + Reserva.Nombre +"</td></tr>";
 		cadena+= "<tr><td>DUI:</td><td>" + Reserva.Dui +"</td></tr>";
 		cadena+= "<tr><td>Numero de Telefono:</td><td>" + Reserva.Telefono +"</td></tr>";
@@ -45,7 +53,37 @@ function iniciar () {
 		cadena+= "<tr><td>Fecha de Reserva:</td><td>" + Reserva.fecha +"</td></tr>";
 		cadena+= "<tr><td>Horario de reservacion:</td><td>"+ hor +"</td></tr></tbody></table>";
 		document.getElementById("tablaGenerar").innerHTML=cadena;
-		document.getElementById("total").innerHTML= "Total de Reservas registradas al momento: "+ total + "."
+		//document.getElementById("total").innerHTML= "Total de Reservas registradas al momento: "+ total + "."
 }
 
+function todasResservaciones () {
+	var cadena= "<table id= \"DatosTotales\"><thead><tr><th colspan=\"4\">Datos de todas las Reservaciones Realizada</th></tr></thead><tbody>";
+		cadena+= "<tr><td>#</td><td>Numero de Mesa</td><td>Fecha de Reservacion:</td><td>Horario:</td></tr>";
+		for (var i = 0; i < total; i++) {
+			var hor="";
+					switch(Reservas[i].Horario) {
+				    case 1:
+				        hor="11:00 am - 12:30 pm.";
+				        break;
+				    case 2:
+				        hor="12:30 pm - 2:00 pm.";
+				        break;
+				    case 3:
+				        hor="6:00 pm - 7:30 pm.";
+				        break;
+				    case 4:
+				        hor="7:30 pm - 9:00 pm.";
+				        break;            
+				    case 5:
+				        hor="9:00 pm - 10:30 pm.";
+				        break;
+				    default:
+				         hor="11:00 am - 12:30 pm.";
+				         break;
+					}
+					cadena+= "<tr><td>"+ (i+1) + "</td><td>"+Reservas[i].NumeroMesa +"</td><td>"+ Reservas[i].fecha+"</td><td>"+ hor+"</td></tr>";
 
+		};
+		cadena+= "</tbody></table>";
+		document.getElementById("TablaTodas").innerHTML=cadena;
+}
